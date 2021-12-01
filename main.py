39596490df41da76606b860553bb73ecf7297f8b
Jackson <jacksonbowe98@gmail.com>
2021-12-02 09:28:11 +1000

    Planning

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 
 
-from roles.actor import Actor
+from engine.roles.actor import Actor
 
 
 a = Actor()
+
+
+
+
+'''
+Game Builder:
+    This should take in a GameSave and a PlayerList, the output should be a GameState
+
+GameState:
+    This object should represent the complete state of a game
+    {
+        day: int,
+        actors: [
+            {
+                number: int,
+                userid: str,
+                nickname: str,
+                role: str (to be converted to object on Game.ResolveState)
+                target: [] (can be a list in event of two-target night actions),
+                house: int (what house is the actor at on this night),
+                
+            }
+        ], ...
+    }
+'''
